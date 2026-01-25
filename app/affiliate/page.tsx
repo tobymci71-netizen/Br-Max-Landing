aaddd54eda940e80578944fbc1241066ea749726
Saud <Saaud266@gmail.com>
2026-01-25 17:57:03 +0530

    Winners annoucement change

--- app/affiliate/page.tsx
+++ app/affiliate/page.tsx
@@ -385,7 +385,7 @@ export default function AffiliatePage() {
                 <svg className="w-5 h-5 text-accent-secondary" fill="currentColor" viewBox="0 0 20 20">
                   <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-12a1 1 0 10-2 0v4a1 1 0 00.293.707l2.828 2.829a1 1 0 101.415-1.415L11 9.586V6z" clipRule="evenodd" />
                 </svg>
-                <span>Winners announced on <span className="font-semibold text-foreground">February 1st</span> in Discord</span>
+                <span>Winners announced in <span className="font-semibold text-foreground">Discord</span></span>
               </motion.div>
             </motion.div>
           </div>
